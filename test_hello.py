d98f4eca4db1e8e216ed7c37836a5d0bc655c680
Alex-Zozulia-1997 <oleksandr97zozulia@gmail.com>
2024-04-24 17:31:00 +0200

    fof

--- test_hello.py
+++ test_hello.py
@@ -10,12 +10,7 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Hello, World!", response.data)
-
-    def test_hello_world(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Hello, World!", response.data)
+        self.assertIn(b"Hello, Flask", response.data)
 
 
 if __name__ == "__main__":
